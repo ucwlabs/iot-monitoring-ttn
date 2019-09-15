187a7a35e9ad84b446e4da398b523ad17076f939
Tomas Hrdlicka <tomas@hrdlicka.co.uk>
2019-09-15 16:55:21 +0200

    Updated example

--- ttn-bridge/app.js
+++ ttn-bridge/app.js
@@ -1,3 +1,30 @@
+/*
+ * Environment monitoring with The Things Network, InfluxDB, and Grafana
+ * TTN Bridge
+ * Copyright 2019 HackTheBase - UCW Labs Ltd. All rights reserved.
+ *
+ * The example was created for the HackTheBase IoT Hub Lab.
+ *
+ * The HackTheBase IoT Hub Lab is a creative space dedicated to prototyping and inventing,
+ * all sorts of microcontrollers (Adafruit Feather, Particle Boron, ESP8266, ESP32 and more)
+ * with various types of connectivity (WiFi, LoRaWAN, GSM, LTE-M), and tools such as screwdriver,
+ * voltmeter, wirings, as well as an excellent program filled with meetups, workshops,
+ * and hackathons to the community members.
+ *
+ * https://hackthebase.com/iot-hub-lab
+ *
+ * The HackTheBase IoT Hub will give you access to the infrastructure dedicated to your project,
+ * and you will get access to our Slack community.
+ *
+ * https://hackthebase.com/iot-hub
+ *
+ * As a member, you will get access to the hardware and software resources
+ * that can help you to work on your IoT project.
+ *
+ * https://hackthebase.com/register
+ *
+ */
+
 import { data } from "ttn";
 const Influx = require("influx");
 
